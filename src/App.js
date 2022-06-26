6f60734c64113215d3e17c418c0d41db1ef15794
Diego Aron <diegobarraza@outlook.com>
2022-06-26 18:58:03 -0400

    Add Wen3 instance
    Add web3 instance, supporter chain ID and copy new pages.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './views/home/index.js';
+import MainLayout from './layouts/main';
 
 function App() {
   return (
     <>
+    <MainLayout>
     <Routes>
       <Route path="/" element={<Home />}/>
     </Routes>
+    </MainLayout>
     </>
   );
 }
