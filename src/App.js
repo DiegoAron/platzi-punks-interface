96f3fc6dfcb4d2bb13046496aa0cbecf92d19bfd
Diego Aron <diegobarraza@outlook.com>
2022-07-06 00:02:45 -0400

    Add singular Page
    Add page for singular tokenID and next token id on home. Add transfer option for the owner account.

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './views/home/index.js';
+import Punks from './views/punks'
+import Punk from './views/punk'
+import MainLayout from './layouts/main';
+
+function App() {
+  return (
+    <>
+    <MainLayout>
+    <Routes>
+    <Route path="/" exact element={<Home />}/>
+    <Route path="/punks" exact element={<Punks/>} />
+    <Route path="/punks/:tokenId" exact element={<Punk/>} />
+   
+    </Routes>
+    </MainLayout>
+    </>
+  );
+}
+
+export default App;
